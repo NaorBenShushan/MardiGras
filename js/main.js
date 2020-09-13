a25f6c190803310e292353f4d263cefbe71c2422
Naor Ben Shushan <nbs257@gmail.com>
2020-09-13 14:27:30 +0300

    Responsive Changes
    13/9/20 At noon

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function innerLinks() {
+  let x = document.getElementById("innerLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
